4011dce1b0cd14f4426016ea292e7b66f0778d23
laurenpearse <laurenpearse@icloud.com>
2025-09-25 14:47:49 +0200

    Simple python v1

---
+++ # A very simple Python program.py
@@ -0,0 +1,7 @@
+# A very simple Python program
+def say_hi(name="there"):
+    print(f"Hi, {name}! 👋")
+
+if __name__ == "__main__":
+    say_hi()          # prints: Hi, there!
+    say_hi("Lauren")  # prints: Hi, Lauren!
